44bc515670cf1cecb2294540ec69ee87c772f2e7
Andrew Cen <andrew.cen@binus.ac.id>
2019-06-18 00:25:07 +0700

    Updated fonts and toolbars

--- app/src/main/java/com/android/cen/andrew/letsdrone/InboxFragment.java
+++ app/src/main/java/com/android/cen/andrew/letsdrone/InboxFragment.java
@@ -7,14 +7,20 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 public class InboxFragment extends Fragment {
+    private Toolbar mToolbar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_inbox, container, false);
+
+        mToolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getContext()).setSupportActionBar(mToolbar);
 
         return view;
     }
