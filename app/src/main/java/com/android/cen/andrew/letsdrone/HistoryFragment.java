d126e0814952ecd2de5201fc43f5388c0cbe8307
Andrew Cen <andrew.cen@binus.ac.id>
2019-06-17 23:00:28 +0700

    Update history fragment design

--- app/src/main/java/com/android/cen/andrew/letsdrone/HistoryFragment.java
+++ app/src/main/java/com/android/cen/andrew/letsdrone/HistoryFragment.java
@@ -7,14 +7,20 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 public class HistoryFragment extends Fragment {
+    private Toolbar mToolbar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        mToolbar = view.findViewById(R.id.toolbar_history);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
 
         return view;
     }
